9a0865da9d5c0d1f7293310a07156e6d3c81ffbc
Rohith patel <rohithgithub999@gmail.com>
2023-07-18 14:19:26 +0000

    es6 named exports

---
+++ importMultipleValues.mjs
@@ -0,0 +1,8 @@
+//importMultipleValues.mjs
+
+import { myArray } from "./exportMultipleValues.mjs";
+import { bulb } from "./exportMultipleValues.mjs";
+import { multiplyByFour } from "./exportMultipleValues.mjs";
+console.log(multiplyByFour(2));
+console.log(myArray);
+console.log(bulb);
